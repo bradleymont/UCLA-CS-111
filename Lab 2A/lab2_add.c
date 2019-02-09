fd75a8440ec2ef5c65cf4387ee8599989711e99d
bradleymont <bradleymont@gmail.com>
2019-02-09 00:32:20 -0800

    implemented add-none

--- Lab 2A/lab2_add.c
+++ Lab 2A/lab2_add.c
@@ -7,8 +7,12 @@
 #include <getopt.h> //for getopt_long(3)
 #include <stdlib.h> //for atoi
 #include <string.h> //for atoi
+#include <stdio.h> //for fprintf(3)
 
-#include <stdio.h> //for fprintf(3), stderror(3)
+//global variables
+long long counter = 0;
+int numThreads = 1;
+int numIterations = 1;
 
 void add(long long *pointer, long long value)
 {
@@ -16,14 +20,27 @@ void add(long long *pointer, long long value)
     *pointer = sum;
 }
 
+void addCaller()
+{
+    for (int i = 0; i < numIterations; i++)
+    {
+        add(&counter, 1);
+    }
+    for (int i = 0; i < numIterations; i++)
+    {
+        add(&counter, -1);
+    }
+}
+
 int main(int argc, char **argv)
 {
+    //note the (high resolution) starting time for the run
+    struct timespec startTime;
+    clock_gettime(CLOCK_MONOTONIC, &startTime);
+    
+    int start = (startTime.tv_sec * 1000000000) + startTime.tv_nsec;
+    
     int exitStatus = 0;
-    
-    long long counter = 0;
-    
-    int numThreads = 1;
-    int numIterations = 1;
     
     static struct option long_options[] =
     {
@@ -62,8 +79,55 @@ int main(int argc, char **argv)
         }
     }
     
-    printf("threads: %d\n", numThreads);
-    printf("iterations: %d\n", numIterations);
+    //array of threads
+    pthread_t* threads = malloc(numThreads * sizeof(pthread_t));
+    
+    //create each thread
+    for (int i = 0; i < numThreads; i++)
+    {
+        //thread, attributes, routine to be executed, argument passed to routine
+        int createReturn = pthread_create(&threads[i], NULL, (void *) &addCaller, NULL);
+        if (createReturn)   //if an error occurred creating the thread
+        {
+            fprintf(stderr, "Error creating thread #%d\n", i + 1);
+            fflush(stderr);
+            exitStatus = 1;
+            //check if exit status equals 1 or 2
+        }
+    }
+    
+    //wait for all threads to complete
+    for (int i = 0; i < numThreads; i++)
+    {
+        int joinReturn = pthread_join(threads[i], NULL);
+        if (joinReturn)   //if an error occurred creating the thread
+        {
+            fprintf(stderr, "Error joining thread #%d\n", i + 1);
+            fflush(stderr);
+            exitStatus = 1;
+            //check if exit status equals 1 or 2
+        }
+    }
+    
+    //note the (high resolution) end time
+    struct timespec endTime;
+    clock_gettime(CLOCK_MONOTONIC, &endTime);
+    
+    int end = (endTime.tv_sec * 1000000000) + endTime.tv_nsec;
+    
+    //print CSV record
+    //name of the test, numThreads, numIterations, operationsPerformed, run time, avg time per operation, total at the end
+    char* testName = "add-none";
+    int numOperations = numThreads * numIterations * 2;
+    int runTime = end - start;
+    int avgTimePerOperation = runTime / numOperations;
+    
+    printf("%s,%d,%d,%d,%d,%d,%lld", testName, numThreads, numIterations, numOperations, runTime, avgTimePerOperation, counter);
+    printf("\n");
+    fflush(stdout);
+    
+    //free dynamically allocated memory
+    free(threads);
     
     return exitStatus;
 }
